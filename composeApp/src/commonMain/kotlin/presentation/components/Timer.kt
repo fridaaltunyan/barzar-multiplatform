2f7a2454223bab08ca314ee0e51b2b63a08c19d1
fridaaltunyan <fridaaltunyan626@gmail.com>
2024-06-15 23:28:29 +0400

    Added needed packages

---
+++ composeApp/src/commonMain/kotlin/presentation/components/Timer.kt
@@ -0,0 +1,38 @@
+package presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TimerBox() {
+    Box(
+        modifier = Modifier.width(width = 100.dp)
+            .clip(shape = RoundedCornerShape(16.dp))
+            .background(Color.Red)
+            .padding(16.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "text")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "text")
+        }
+    }
+}
+@Preview
+@Composable
+fun TimerBoxPreview() {
+    TimerBox()
+}
